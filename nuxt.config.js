9fa4eebb2b6f2dd4efc0f78f20bc9944406d605b
SakuraCase <sakurada6617@gmail.com>
2018-07-02 00:05:46 +0900

    remove pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js tutorial' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -45,12 +45,7 @@ module.exports = {
   modules: [
     '@nuxtjs/axios',
     '@nuxtjs/bulma',
-    '@nuxtjs/pwa',
   ],
-  manifest: {
-      name: 'PWA',
-      lang: 'ja'
-  },
   watchers: {
     webpack: {
       poll: true
